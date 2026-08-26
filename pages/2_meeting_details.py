3e25f58493cb4ca419d5051d584709b571b26d36
pyscriptcli <davepolicarpio.r@gmail.com>
2026-08-26 11:39:50 +0800

    Update 2_meeting_details.py

--- pages/2_meeting_details.py
+++ pages/2_meeting_details.py
@@ -3,6 +3,10 @@ import datetime
 import pandas as pd
 import streamlit as st
 from supabase import create_client, Client
+from navigation import render_global_navbar
+
+st.set_page_config(page_title="Project Echo", layout="wide", initial_sidebar_state="collapsed")
+render_global_navbar()
 
 # ========== CONFIG ==========
 st.set_page_config(
