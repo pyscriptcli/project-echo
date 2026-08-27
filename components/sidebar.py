1074a70703ad36af9bd1218c84e76d042ab9c0df
pyscriptcli <davepolicarpio.r@gmail.com>
2026-08-27 11:17:28 +0800

    Update sidebar.py

--- components/sidebar.py
+++ components/sidebar.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 def render_custom_sidebar():
     # Navigation Links (Material Icons act as SVGs)
-    st.page_link("app.py", label="Executive Dashboard", icon=":material/dashboard:")
-    st.page_link("pages/1_minutes_of_the_meeting.py", label="MoM Generator", icon=":material/edit_note:")
-    st.page_link("pages/2_meeting_details.py", label="Meeting Browser", icon=":material/menu_book:")
+    st.page_link("app.py", label="Dashboard", icon=":material/dashboard:")
+    st.page_link("pages/2_meeting_details.py", label="Meetings", icon=":material/menu_book:")
+    st.page_link("pages/1_minutes_of_the_meeting.py", label="Minutes of the Meeting", icon=":material/edit_note:")
+
