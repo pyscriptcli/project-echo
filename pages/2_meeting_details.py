9bd0dc09626bd50db8af730ecdc6b5efb168355e
pyscriptcli <davepolicarpio.r@gmail.com>
2026-08-27 11:41:35 +0800

    Update 2_meeting_details.py

--- pages/2_meeting_details.py
+++ pages/2_meeting_details.py
@@ -89,9 +89,6 @@ div[data-testid="stVerticalBlockBorderWrapper"] {
 """
 st.markdown(CUSTOM_CSS, unsafe_allow_html=True)
 
-# 3. Render Custom Sidebar
-render_custom_sidebar()
-
 # 4. SVG Icons (No Emojis)
 TRASH_ICON = '<svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor" style="vertical-align: middle; margin-right: 4px;"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>'
 SAVE_ICON = '<svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor" style="vertical-align: middle; margin-right: 6px;"><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>'
