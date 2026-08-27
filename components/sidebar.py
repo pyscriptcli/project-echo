db38da67023aca360ee5312ff1ffb1c520df17f8
pyscriptcli <davepolicarpio.r@gmail.com>
2026-08-27 11:23:19 +0800

    Update sidebar.py

--- components/sidebar.py
+++ components/sidebar.py
@@ -5,6 +5,3 @@ def render_custom_sidebar():
     st.page_link("app.py", label="Dashboard", icon=":material/dashboard:")
     st.page_link("pages/2_meeting_details.py", label="Meetings", icon=":material/menu_book:")
     st.page_link("pages/1_minutes_of_the_meeting.py", label="Minutes of the Meeting", icon=":material/edit_note:")
-
-
-make the navigation links to show in one horizontal row 
